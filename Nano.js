a26c3b2f6caa498485e6765f2a4fb1dace2051d5
Foxter28 <157205566+Lucyy28@users.noreply.github.com>
2025-02-08 09:46:56 +0000

    update

--- Nano.js
+++ Nano.js
@@ -27839,33 +27839,6 @@ case 'bass': case 'blown': case 'deep': case 'earrape': case 'fast': case 'fat':
         replynano(e);
     }
     break
-case 'define': 
-if (!q) return replynano(`What do you want to define?`)
-try {
-targetfine = await axios.get(`http://api.urbandictionary.com/v0/define?term=${q}`)
-if (!targetfine) return replynano(mess.error)
-let reply =`
-*${themeemoji} Word:* ${q}
-*${themeemoji} Definition:* ${targetfine.data.list[0].definition
-    .replace(/\[/g, "")
-    .replace(/\]/g, "")}
-*${themeemoji} Example:* ${targetfine.data.list[0].example
-    .replace(/\[/g, "")
-    .replace(/\]/g, "")}`
-   NanoBotz.sendMessage(m.chat,{text:reply},{quoted:m})
-} catch (err) {
-    console.log(err)
-    return replynano(`*${q}* isn't a valid text`)
-    }
-    break
-case 'rate': {
-            	if (!text) return replynano(`Example : ${prefix + command} my profile`)
-            	let ra = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
-let kah = ra[Math.floor(Math.random() * ra.length)]
-let jawab = `*Rate ${text}*\nAnswer : ${kah}%`
-            await replynano(jawab)
-            }
-            break
             case 'runtime': {
             	let lowq = `*The Bot Has Been Online For:*\n*${runtime(process.uptime())}*`
 replynano(lowq)
